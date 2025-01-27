041f6a14ec6dbc3bc2f09cf8dfdd073b5ee685dc
ItaloCabral1995RN <italocabral95@gmail.com>
2025-01-27 06:02:00 -0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Bem-vindo à Câmara Municipal</h1>
+      <p>Este é o painel de informações!</p>
+    </div>
+  );
+}
+
+export default App;
